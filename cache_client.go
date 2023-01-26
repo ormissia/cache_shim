ba5ec9cfbebab87aa0605a298c222063e1c552c2
安红豆 <ormissia@outlook.com>
2023-01-27 00:28:22 +0800

    :beers: 更新注释

--- cache_client.go
+++ cache_client.go
@@ -2,6 +2,7 @@ package cache_shim
 
 import "sync"
 
+// CacheClint 缓存客户端接口定义
 type CacheClint interface {
 	Del(key string) (int64, error)
 	SetString(key, value string, expire int) error
--- entity.go
+++ entity.go
@@ -1,5 +1,6 @@
 package cache_shim
 
+// CacheType 需要缓存的实体接口定义
 type CacheType interface {
 	CacheKey() string
 	Expiration() int
